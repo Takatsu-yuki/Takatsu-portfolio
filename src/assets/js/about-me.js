3cb516646b3526211b4715ddcc53c78ca7da7bb1
Takatsu-yuki <yk.neve@gmail.com>
2025-03-04 19:06:32 +0900

    about me スタイル中

--- src/assets/js/about-me.js
+++ src/assets/js/about-me.js
@@ -1,1 +1,32 @@
 "use strict";
+
+//yuki takatsu
+const slides = document.querySelectorAll('#yuki [class^="slide-"]');
+const subheads = document.querySelectorAll("#yuki .subhead");
+let lastScrollY = 0;
+
+//Observerオプション
+const yukiOptions = {
+  root: null,
+  rootMargin: `${-window.innerHeight / 2 + 300}px 0px ${-window.innerHeight / 2 + 300}px 0px`,
+  threshold: 1,
+};
+
+const yukiObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    } else {
+      if (lastScrollY > window.scrollY) {
+        entry.target.classList.remove("active");
+      }
+    }
+    console.log(lastScrollY);
+  });
+  lastScrollY = window.scrollY;
+}, yukiOptions);
+
+//監視対象
+slides.forEach((slide) => {
+  yukiObserver.observe(slide);
+});
